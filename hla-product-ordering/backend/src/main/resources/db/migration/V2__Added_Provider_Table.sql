9b2949cc2c0c5ff392cbc75805a77e8306f53f0c
Elvira-91 <elviramjeshtri@yahoo.com>
2023-07-10 15:09:55 +0200

    backend | flyway | Update table names to match entities

--- hla-product-ordering/backend/src/main/resources/db/migration/V2__Added_Provider_Table.sql
+++ hla-product-ordering/backend/src/main/resources/db/migration/V2__Added_Provider_Table.sql
@@ -1,6 +1,6 @@
 create sequence provider_id_seq start with 1 increment by 1;
 
-create table provider
+create table provider_entity
 (
     id         integer                     not null,
     email      varchar(255)                not null,
