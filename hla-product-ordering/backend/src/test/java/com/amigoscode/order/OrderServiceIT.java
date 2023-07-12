1eaadfd0bf8c940ca085bb29b5b7cc14f78f91b5
tuongtran23 <tuongtran1231@gmail.com>
2023-07-12 16:35:38 -0400

    solve problem with OrderServiceIT

--- hla-product-ordering/backend/src/test/java/com/amigoscode/order/OrderServiceIT.java
+++ hla-product-ordering/backend/src/test/java/com/amigoscode/order/OrderServiceIT.java
@@ -2,15 +2,11 @@ package com.amigoscode.order;
 
 import com.amigoscode.BaseIT;
 import com.amigoscode.TestOrderFactory;
-import com.amigoscode.TestUserFactory;
 import com.amigoscode.domain.order.Order;
 import com.amigoscode.domain.order.OrderService;
-import com.amigoscode.domain.user.User;
-import com.amigoscode.domain.user.UserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class OrderServiceIT extends BaseIT {
 
@@ -24,17 +20,17 @@ public class OrderServiceIT extends BaseIT {
         Order order2 = TestOrderFactory.create();
         Order order3 = TestOrderFactory.create();
 
-        service.save(order1);
-        service.save(order2);
-        service.save(order3);
+        Order savedOrder1 = service.save(order1);
+        Order savedOrder2 = service.save(order2);
+        Order savedOrder3 = service.save(order3);
 
-//        //when
-        Order readOrder = service.findById(order2.getId());
+        //when
+        Order readOrder = service.findById(savedOrder2.getId());
 
-//        //then
+        //then
         Assertions.assertEquals(order2.getScheduleId(), readOrder.getScheduleId());
         Assertions.assertEquals(order2.getScheduleVersion(), readOrder.getScheduleVersion());
-        Assertions.assertEquals(order2.getScheduledFor(), readOrder.getScheduledFor());
+        Assertions.assertEquals(order2.getScheduledFor().toInstant(), readOrder.getScheduledFor().toInstant());
         Assertions.assertEquals(order2.getEmailId(), readOrder.getEmailId());
     }
 }
