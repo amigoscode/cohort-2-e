5290a02c254365c9c84e1afab20513c9a16bed02
emj <elvira@codefriends.ch>
2023-08-12 14:10:08 +0200

    backend | Code refactor

--- hla-product-ordering/backend/src/test/java/com/amigoscode/domain/schedule/ScheduleServiceIT.java
+++ hla-product-ordering/backend/src/test/java/com/amigoscode/domain/schedule/ScheduleServiceIT.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.format.DateTimeFormatter;
+
 public class ScheduleServiceIT extends BaseIT {
 
     @Autowired
@@ -15,6 +17,7 @@ public class ScheduleServiceIT extends BaseIT {
 
     @Test
     void add_schedule_test() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
         //given
         User user = TestUserFactory.createTechnologist();
         User savedUser = userService.save(user);
@@ -24,8 +27,10 @@ public class ScheduleServiceIT extends BaseIT {
         Schedule readSchedule = scheduleService.findById(savedSchedule.getId());
 
         //then
-        Assertions.assertEquals(savedSchedule.getVersion(), readSchedule.getVersion());
-        Assertions.assertEquals(savedSchedule.getNote(), readSchedule.getNote());
+        Assertions.assertEquals(savedSchedule.getVersion().getId(), readSchedule.getVersion().getId());
+        Assertions.assertEquals(savedSchedule.getVersion().getUpdatedAt().format(formatter), readSchedule.getVersion().getUpdatedAt().format(formatter));
+        Assertions.assertEquals(savedSchedule.getNote().getId(), readSchedule.getNote().getId());
+        Assertions.assertEquals(savedSchedule.getNote().getCreatedAt().format(formatter), readSchedule.getNote().getCreatedAt().format(formatter));
         Assertions.assertEquals(savedSchedule.getPatientId(), readSchedule.getPatientId());
     }
 
@@ -45,17 +50,19 @@ public class ScheduleServiceIT extends BaseIT {
 
         //when
         Schedule readSchedule = scheduleService.findById(savedSchedule2.getId());
-
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
         //then
         Assertions.assertEquals(savedSchedule2.getVersion().getId(), readSchedule.getVersion().getId());
-        Assertions.assertEquals(savedSchedule2.getVersion().getUpdatedAt().toInstant(), readSchedule.getVersion().getUpdatedAt().toInstant());
-        Assertions.assertEquals(savedSchedule2.getNote(), readSchedule.getNote());
+        Assertions.assertEquals(savedSchedule2.getVersion().getUpdatedAt().format(formatter), readSchedule.getVersion().getUpdatedAt().format(formatter));
+        Assertions.assertEquals(savedSchedule2.getNote().getId(), readSchedule.getNote().getId());
+        Assertions.assertEquals(savedSchedule2.getNote().getCreatedAt().format(formatter), readSchedule.getNote().getCreatedAt().format(formatter));
         Assertions.assertEquals(savedSchedule2.getPatientId(), readSchedule.getPatientId());
         Assertions.assertEquals(savedSchedule2.getStatus(), readSchedule.getStatus());
     }
 
     @Test
     void update_schedule_test() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
         //given
         User user = TestUserFactory.createTechnologist();
         User savedUser = userService.save(user);
@@ -74,8 +81,9 @@ public class ScheduleServiceIT extends BaseIT {
 
         //then
         Assertions.assertEquals(2, readSchedule.getPatientId());
-        Assertions.assertEquals(scheduleToUpdate.getVersion(), readSchedule.getVersion());
-        Assertions.assertEquals(scheduleToUpdate.getNote(), readSchedule.getNote());
+        Assertions.assertEquals(scheduleToUpdate.getVersion().getUpdatedAt().format(formatter), readSchedule.getVersion().getUpdatedAt().format(formatter));
+        Assertions.assertEquals(scheduleToUpdate.getNote().getId(), readSchedule.getNote().getId());
+        Assertions.assertEquals(scheduleToUpdate.getNote().getCreatedAt().format(formatter), readSchedule.getNote().getCreatedAt().format(formatter));
         Assertions.assertEquals(scheduleToUpdate.getPatientId(), readSchedule.getPatientId());
     }
 
